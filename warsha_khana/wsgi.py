c0b475f1aec483e63ad74a49a35ba83eeb938646
Islam Bayoumy <islam.bayomy@gmail.com>
2017-03-24 17:06:38 +0200

    Start project

---
+++ warsha_khana/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for warsha_khana project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "warsha_khana.settings")
+
+application = get_wsgi_application()
